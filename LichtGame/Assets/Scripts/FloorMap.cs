c9f63d356c450782ce585f4e9f8ccdd58346e353
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-08-31 23:34:34 -0400

    comments + maze logic
    lots and lots of comments. read for better idea on design+dev of maze layout.
    
    - implement algo + edge cases
    - room object script
    - make sure vars are saved/updated properly

--- LichtGame/Assets/Scripts/FloorMap.cs
+++ LichtGame/Assets/Scripts/FloorMap.cs
@@ -1,7 +1,32 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public class FloorMap : MonoBehaviour
 {
+    // dont explicitly use ints for number rooms. 
+    // room is found by taking each individual digit of currentRoom# inputted,
+    // so room 11 is row1, col1 for dungeon[][], etc.
+    // then, each room stores a Room object, which have 
+    // .type (int) for attacking, .roomNumber (int) to double check + convert to pixels for spawning,
+    // and .already (int) for if already visited room, and what direction you left in --> for gen after-image
+    // so we can call dungeon[room#Tens][room#Ones].var for values
+
+    // DS for rooms --> list of lists
+    List<List<int>> dungeon = new List<List<int>>
+    { 
+        new List<int> {11, 12, 13, 14, 15, 16, 17, 18, 19},
+        new List<int> {21, 22, 23, 24, 25, 26, 27, 28, 29},
+        new List<int> {31, 32, 33, 34, 35, 36, 37, 38, 39},
+        new List<int> {41, 42, 43, 44, 45, 46, 47, 48, 49},
+        new List<int> {51, 52, 53, 54, 55, 56, 57, 58, 59},
+        new List<int> {61, 62, 63, 64, 65, 66, 67, 68, 69},
+        new List<int> {71, 72, 73, 74, 75, 76, 77, 78, 79},
+        new List<int> {81, 82, 83, 84, 85, 86, 87, 88, 89},
+        new List<int> {91, 92, 93, 94, 95, 96, 97, 98, 99}
+    };
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,14 +35,40 @@ public class FloorMap : MonoBehaviour
         GenerateDungeon(seed, level);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void GenerateDungeon(int seed, int level)
     {
+        // we want a snake like pattern, so length > variety. 
+        // this leads to streamlined traversals, which gets us:
+        //  more flow, less memorizing, and you FEEL faster.
+
+        // THIS IS NOT AN EXPLORATION GAME! 
+        // it has a story focus, so add lore rooms, 
+        // but more importantly, we want players to want to play, not want to quit
+        // --> streamlined maps, fast/powerful feeling gameplay, but varied experiences.
+
+        // what does this mean? ...
+        // it means:
+        // - keep them thinking, but dont make them think about things they dont need to think abt
         
+        // timer mechanic:
+        // we reset if we dont reach the boss room fast enough.
+        // keep a counter of how many resets, and change final boss dialogue
+        // - /difficulty(very slightly) based on that.
+        //      - then, speedrunners/better players have negative feedback loop (?) [do we even want that?]
+        // - positive feedback loop with perma cosmetic :D or shop creds
+
+        // IMPORTANT: THIS IS NOT A ROGUELIKE
+        // shop items should be temporary one-time usage or perma cosmetic
+        // - if ppl replay the game, i think dialogue should be different the second time
+
+        int numRooms = (int)Math.Floor(UnityEngine.Random.Range(0f, 3f) + 6f + (level * 2.4f));
+
+        int curRooms = 0;
+        List<int> roomsByNum = new List<int>();
+        while (numRooms > curRooms){
+
+            curRooms++;
+            // if last room to generate, make it the boss one
+        }
     }
 }

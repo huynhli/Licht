98697b6166f4a8696ef0f4aa18af24c22c92885c
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-09-02 08:08:38 -0400

    helper dungeon func inprogress

--- LichtGame/Assets/Scripts/FloorMap.cs
+++ LichtGame/Assets/Scripts/FloorMap.cs
@@ -23,33 +23,34 @@ public class FloorMap : MonoBehaviour
     // .type (int) for attacking, .roomNumber (int) to double check + convert to pixels for spawning,
     // and .already (int) for if already visited room, and what direction you left in --> for gen after-image
     // so we can call dungeon[room#Tens][room#Ones].var for values
-    
 
     // DS for rooms --> list of lists
-    List<List<int>> dungeon = new List<List<int>>
-    { 
-        // types of rooms:
-        // 0: starter 
-        // 1: combat
-        // 2: puzzle+combat
-        // 3: puzzle
-        // 4: puzzle+lore
-        // 5: lore
-        // 6: cat shop
-        // always at least one - 7: boss battle 
+    // types of rooms:
+    // 0: starter 
+    // 1: combat
+    // 2: puzzle+combat
+    // 3: puzzle
+    // 4: puzzle+lore
+    // 5: lore
+    // 6: cat shop
+    // always at least one - 7: boss battle 
 
-        // MAKE THESE LEFT SKEW
-        new List<int> {new Room(UnityEngine.Random.Range(0f, 7), 11), 12, 13, 14, 15, 16, 17, 18, 19},
-        new List<int> {21, 22, 23, 24, 25, 26, 27, 28, 29},
-        new List<int> {31, 32, 33, 34, 35, 36, 37, 38, 39},
-        new List<int> {41, 42, 43, 44, 45, 46, 47, 48, 49},
-        new List<int> {51, 52, 53, 54, 55, 56, 57, 58, 59},
-        new List<int> {61, 62, 63, 64, 65, 66, 67, 68, 69},
-        new List<int> {71, 72, 73, 74, 75, 76, 77, 78, 79},
-        new List<int> {81, 82, 83, 84, 85, 86, 87, 88, 89},
-        new List<int> {91, 92, 93, 94, 95, 96, 97, 98, 99}
-    };
+    // MAKE THESE LEFT SKEW
+    List<List<Room>> dungeon = makeFloorMap();
 
+    private List<List<Room>> makeFloorMap()
+    {
+        List<List<Room>> floorMap = new List<List<Room>>();
+        for (int j = 10; i < 100; i += 10)
+        {
+            List<Room> tempListRoom = new List<Room>();
+            for (int i = 1; i < 10; i++)
+            {
+                tempListRoom.append(new Room(UnityEngine.Random.Range(0, 7), i + j));
+            }
+            floorMap.append(tempListRoom);
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -88,9 +89,11 @@ public class FloorMap : MonoBehaviour
         int numRooms = (int)Math.Floor(UnityEngine.Random.Range(0f, 3f) + 6f + (level * 2.4f));
 
         int curRooms = 0;
-        List<int> roomsByNum = new List<int>();
-        while (numRooms > curRooms){
-            
+
+        List<Room> roomStack = new List<Room>();
+        while (numRooms > curRooms)
+        {
+
             curRooms++;
             // if last room to generate, make it the boss one
         }

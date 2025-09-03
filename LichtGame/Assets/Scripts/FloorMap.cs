d47110b014ef433ec34cfca8b21b0fd42bcdf2bb
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-09-03 00:29:07 -0400

    temp - working on map gen

--- LichtGame/Assets/Scripts/FloorMap.cs
+++ LichtGame/Assets/Scripts/FloorMap.cs
@@ -2,16 +2,45 @@ using UnityEngine;
 using System;
 using System.Collections.Generic;
 
-public struct Room {
+public class Room {
     [SerializeField] private int type;
     [SerializeField] private int roomNumber;
     [SerializeField] private int already;
+    [SerializeField] public List<Room> neighbours;
 
-    public Room(int type, int roomNumber){
-        this.type = type;
+    public Room(int roomNumber){
+        this.type = 0;
         this.roomNumber = roomNumber;
         this.already = 0;
+        this.neighbours = new List<Room>();
     } 
+
+    public void initType(bool nearingEnd, bool haveBossRoom, bool lastRoom){         // room gen (mandatory)
+        if (lastRoom) {
+            this.type = 8;
+        } 
+        if (nearingEnd && !haveBossRoom) {
+            this.type = UnityEngine.Random.Range(4, 9);
+        } else if(nearingEnd) {
+            this.type = UnityEngine.Random.Range(4, 8);
+        }   
+        else {
+            this.type = UnityEngine.Random.Range(1, 8);
+        }
+        // TODO skew this
+    }   
+
+    public int getTypeValue(){           // room load
+        return this.type;
+    }
+
+    public int getRoomNumber(){     // room gen(surroundings), room load (bounds)
+        return this.roomNumber;
+    }
+
+    public int getAlready() {
+        return this.already;        // generate map for map key / on re-run
+    }
 }
 
 public class FloorMap : MonoBehaviour
@@ -22,39 +51,63 @@ public class FloorMap : MonoBehaviour
     // then, each room stores a Room object, which have 
     // .type (int) for attacking, .roomNumber (int) to double check + convert to pixels for spawning,
     // and .already (int) for if already visited room, and what direction you left in --> for gen after-image
-    // so we can call dungeon[room#Tens][room#Ones].var for values
+    // so we can call dungeon[room#Tens][room#Ones] for indexing
 
     // DS for rooms --> list of lists
     // types of rooms:
-    // 0: starter 
-    // 1: combat
-    // 2: puzzle+combat
-    // 3: puzzle
-    // 4: puzzle+lore
-    // 5: lore
-    // 6: cat shop
-    // always at least one - 7: boss battle 
+    // 0: null
+    // 1: starter 
+    // 2: combat
+    // 3: puzzle+combat
+    // 4: puzzle
+    // 5: puzzle+lore
+    // 6: lore
+    // 7: cat shop
+    // always at least one - 8: boss battle 
 
     // MAKE THESE LEFT SKEW
-    List<List<Room>> dungeon = makeFloorMap();
-
-    private List<List<Room>> makeFloorMap()
+    List<List<Room>> dungeon;
+    
+    void PrintDungeon()
     {
-        List<List<Room>> floorMap = new List<List<Room>>();
-        for (int j = 10; i < 100; i += 10)
+        for (int row = 0; row < dungeon.Count; row++)
         {
-            List<Room> tempListRoom = new List<Room>();
-            for (int i = 1; i < 10; i++)
+            string line = "";
+            for (int col = 0; col < dungeon[row].Count; col++)
             {
-                tempListRoom.append(new Room(UnityEngine.Random.Range(0, 7), i + j));
+                Room r = dungeon[row][col];
+                // prints type or 0 if uninitialized
+                line += r.getTypeValue() + " ";
             }
-            floorMap.append(tempListRoom);
+            Debug.Log(line);
         }
     }
+    
+    // TODO: dont need rooms for every empty room
+    List<List<Room>> makeFloorMap()
+    {
+        List<List<Room>> floorMap = new List<List<Room>>();
+
+        for (int row = 0; row < 10; row++)
+        {
+            List<Room> tempListRoom = new List<Room>();
+            for (int col = 0; col < 10; col++)
+            {
+                int roomNum = row * 10 + col;
+                tempListRoom.Add(new Room(roomNum));
+            }
+            floorMap.Add(tempListRoom);
+        }
+
+        return floorMap;
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        dungeon = makeFloorMap();
+        PrintDungeon();
         int seed = GameManagerSingleton.Instance.dungeonSeed;
         int level = GameManagerSingleton.Instance.level;
         GenerateDungeon(seed, level);
@@ -88,14 +141,74 @@ public class FloorMap : MonoBehaviour
 
         int numRooms = (int)Math.Floor(UnityEngine.Random.Range(0f, 3f) + 6f + (level * 2.4f));
 
-        int curRooms = 0;
+        int numCurRooms = 0;
 
-        List<Room> roomStack = new List<Room>();
-        while (numRooms > curRooms)
-        {
+        // TODO: queue or stack? stack has chance for VERY linear, queue has chance for much more spread
+        // update: queue or balanced avl?
+        // or can have balanced counter like avl trees
 
-            curRooms++;
-            // if last room to generate, make it the boss one
+        // numOfCurRooms = rolling num of rooms generated
+        // curRoom = current room looking at
+        // curRoomNum = nuber of current room number looking at
+
+        // Binding of Isaac
+        // For each cell in the queue, it loops over the 4 cardinal directions and does the following:
+            // Determine the neighbour cell by adding +10/-10/+1/-1 to the currency cell.
+            // If the neighbour cell is already occupied, give up
+            // If the neighbour cell itself has more than one filled neighbour, give up.
+            // If we already have enough rooms, give up
+            // Random 50% chance, give up
+            // Otherwise, mark the neighbour cell as having a room in it, and add it to the queue.
+
+        var roomQueue = new Queue<Room>();
+        Room startRoom = dungeon[5][6]; // example starting point
+        roomQueue.Enqueue(startRoom);
+
+        bool haveBossRoom = false;
+
+        while (numCurRooms < numRooms && roomQueue.Count > 0) {
+            Room curRoom = roomQueue.Dequeue();
+            curRoom.initType(numCurRooms > numRooms/3*2, haveBossRoom, numCurRooms == numRooms-1);
+            if (curRoom.getTypeValue == 9) haveBossRoom = true;
+            numCurRooms++;
+
+            int curRoomNum = curRoom.getRoomNumber();
+            int row = curRoomNum / 10;
+            int col = curRoomNum % 10;
+
+            // directions: right, up, left, down
+            var directions = new List<(int dr, int dc)> {
+                (0, 1), (1, 0), (0, -1), (-1, 0)
+            };
+
+            int addingRooms = 0;
+            foreach (var (dr, dc) in directions) {
+                if (addingRooms > 1) break;
+                int newRow = row + dr;
+                int newCol = col + dc;
+
+                // edge / break case
+                if (newRow < 0 || newRow >= 10 || newCol < 0 || newCol >= 10)
+                    continue;
+
+                Room neighbour = dungeon[newRow][newCol];
+
+                // skip if already filled, has too many neighbours, or random reject
+                if (neighbour.getTypeValue() != 0 ||
+                    neighbour.neighbours.Count > 1 ||
+                    UnityEngine.Random.value < 0.5f) {
+                    continue;
+                }
+
+                // otherwise, connect and add to queue
+                curRoom.neighbours.Add(neighbour);
+                neighbour.neighbours.Add(curRoom);
+                roomQueue.Enqueue(neighbour);
+                addingRooms++;
+            }
+
         }
+        
+        
     }
 }
